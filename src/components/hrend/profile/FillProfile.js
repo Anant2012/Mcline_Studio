8f2ff87ad108cd93815bbbeb70bebd7f83bdd491
Chirag <chiragvalecha97@gmail.com>
2023-02-22 12:05:28 +0530

    edit/view personal details

--- src/components/hrend/profile/FillProfile.js
+++ src/components/hrend/profile/FillProfile.js
@@ -1,6 +1,5 @@
-
-import React, { useState } from 'react'
-import { AxiosInstance } from '../../../AxiosInstance/AxiosInstance';
+import { useState } from "react";
+import { AxiosInstance } from "../../../AxiosInstance/AxiosInstance";
 import { useSelector } from "react-redux";
 
 const FillProfile = (props) => {
@@ -16,27 +15,32 @@ const FillProfile = (props) => {
   const [personal_email_id, setPersonal_email_id] = useState("");
   const [official_email_id, setOfficial_email_id] = useState("");
   const [company_id, setCompany_id] = useState("");
+  const [profileImg, setProfileImg] = useState();
 
   const [isDisabled, setIsDisabled] = useState(false);
 
   const AddPersonalDetails = async (e) => {
     setIsDisabled(true);
     e.preventDefault();
-    const data = {
-      name,
-      employee_code,
-      designation,
-      grade,
-      reporting_manager,
-      emergency_contact_number,
-      blood_group,
-      personal_contact_number,
-      personal_email_id,
-      official_email_id,
-      company_id,
-    }
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("employee_code", employee_code);
+    formData.append("designation", designation);
+    formData.append("grade", grade);
+    formData.append("reporting_manager", reporting_manager);
+    formData.append("emergency_contact_number", emergency_contact_number);
+    formData.append("blood_group", blood_group);
+    formData.append("personal_contact_number", personal_contact_number);
+    formData.append("personal_email_id", personal_email_id);
+    formData.append("official_email_id", official_email_id);
+    formData.append("company_id", company_id);
+    formData.append("profile_image", profileImg);
+
     try {
-      const response = await AxiosInstance.post(`/api/hr/edit/${userId}`, data)
+      const response = await AxiosInstance.post(
+        `/api/hr/edit/${userId}`,
+        formData
+      );
       if (response.status === 200) {
         alert("✅ Profile Added SuccesFully");
       }
@@ -45,7 +49,7 @@ const FillProfile = (props) => {
       setOfficial_email_id("");
       setCompany_id("");
       setEmployee_code("");
-      setReporting_manager("")
+      setReporting_manager("");
       setDesignation("");
       setBlood_group("");
       setEmergency_contact_number("");
@@ -53,15 +57,13 @@ const FillProfile = (props) => {
       setGrade("");
     } catch (error) {
       alert(error.response.data.msg);
-
-    };
-  }
-
+    }
+  };
 
   const ProfileImageClick = (props) => {
     const imgBtn = document.querySelector("#imgBtn");
     imgBtn.click();
-  }
+  };
 
   // const onSubmitClick = () => {
   //   AddPersonalDetails();
@@ -70,7 +72,7 @@ const FillProfile = (props) => {
 
   return (
     <>
-      <div className={`${(props.viewFillProfile) ? "block" : "hidden"}`}>
+      <div className={`${props.viewFillProfile ? "block" : "hidden"}`}>
         <div class="flex flex-col text-center w-full my-8">
           <h1 class="sm:text-4xl text-3xl font-medium title-font text-gray-900">
             Fill your Details
@@ -81,7 +83,10 @@ const FillProfile = (props) => {
             <div className="flex justify-between items-center">
               <div>
                 <div class="relative flex flex-row  items-center">
-                  <label for="date" class="text-sm font-medium mr-2 text-gray-500">
+                  <label
+                    for="date"
+                    class="text-sm font-medium mr-2 text-gray-500"
+                  >
                     Name
                   </label>
                   <input
@@ -97,12 +102,23 @@ const FillProfile = (props) => {
 
               <div class="center h-full flex items-center justify-center">
                 <div class="form-input w-[350px]  flex justify-end bg-white">
-                  <label for="file-ip-1" className=' block w-1/2 leading-10 text-center bg-[#1172c2] text-[15px] uppercase font-semibold cursor-pointer rounded-[5px] text-white' onClick={(e) => {
-                    e.preventDefault();
-                    ProfileImageClick();
-                    console.log("Clicked");
-                  }}>Upload Image</label>
-                  <input className='file-upload-input hidden' id="imgBtn" type="file" accept="Image/"
+                  <label
+                    for="file-ip-1"
+                    className=" block w-1/2 leading-10 text-center bg-[#1172c2] text-[15px] uppercase font-semibold cursor-pointer rounded-[5px] text-white"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      ProfileImageClick();
+                      console.log("Clicked");
+                    }}
+                  >
+                    Upload Image
+                  </label>
+                  <input
+                    className="file-upload-input hidden"
+                    id="imgBtn"
+                    type="file"
+                    accept="Image/"
+                    onChange={(e) => setProfileImg(e.target.files[0])}
                   />
                 </div>
               </div>
@@ -150,7 +166,9 @@ const FillProfile = (props) => {
                 </dd>
               </div>
               <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt class="text-sm font-medium text-gray-500">Reporting Manager</dt>
+                <dt class="text-sm font-medium text-gray-500">
+                  Reporting Manager
+                </dt>
                 <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
                   <input
                     type="text"
@@ -171,7 +189,9 @@ const FillProfile = (props) => {
                     type="number"
                     id="number"
                     value={emergency_contact_number}
-                    onChange={(e) => setEmergency_contact_number(e.target.value)}
+                    onChange={(e) =>
+                      setEmergency_contact_number(e.target.value)
+                    }
                     name="number"
                     class="w-3/4 sm:w-1/2 bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 px-3 leading-8 transition-colors duration-200 ease-in-out"
                   />
@@ -206,7 +226,9 @@ const FillProfile = (props) => {
                 </dd>
               </div>
               <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt class="text-sm font-medium text-gray-500">Personal Email ID</dt>
+                <dt class="text-sm font-medium text-gray-500">
+                  Personal Email ID
+                </dt>
                 <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
                   <input
                     type="email"
@@ -219,7 +241,9 @@ const FillProfile = (props) => {
                 </dd>
               </div>
               <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt class="text-sm font-medium text-gray-500">Official Email ID</dt>
+                <dt class="text-sm font-medium text-gray-500">
+                  Official Email ID
+                </dt>
                 <dd class="mt-1 t ext-sm text-gray-900 sm:col-span-2 sm:mt-0">
                   <input
                     type="email"
@@ -248,7 +272,14 @@ const FillProfile = (props) => {
           </div>
         </div>
         <div className="w-full flex justify-center">
-          <button onClick={AddPersonalDetails} disabled={isDisabled} style={{ cursor: isDisabled ? "not-allowed" : "pointer" }} className="mx-auto w-11/12 sm:w-3/4 text-white bg-[#047EC1] mt-4 mb-12 border-0 py-2 px-6 focus:outline-none hover:bg-[#0473af] rounded text-lg">Submit</button>
+          <button
+            onClick={AddPersonalDetails}
+            disabled={isDisabled}
+            style={{ cursor: isDisabled ? "not-allowed" : "pointer" }}
+            className="mx-auto w-11/12 sm:w-3/4 text-white bg-[#047EC1] mt-4 mb-12 border-0 py-2 px-6 focus:outline-none hover:bg-[#0473af] rounded text-lg"
+          >
+            Submit
+          </button>
         </div>
       </div>
     </>
--- src/components/hrend/profile/ViewProfile.js
+++ src/components/hrend/profile/ViewProfile.js
@@ -1,71 +1,44 @@
-import React from "react";
 import { useEffect, useState } from "react";
-import { AxiosInstance } from '../../../AxiosInstance/AxiosInstance';
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { AxiosInstance } from "../../../AxiosInstance/AxiosInstance";
+
 const ViewProfile = (props) => {
-  const { userId } = useSelector((state) => state);
-  const [data, setData] = useState();
-  const [name, setName] = useState("");
-  const [employee_code, setEmployee_code] = useState("");
-  const [designation, setDesignation] = useState("");
-  const [grade, setGrade] = useState("");
-  const [reporting_manager, setReporting_manager] = useState("");
-  const [emergency_contact_number, setEmergency_contact_number] = useState("");
-  const [blood_group, setBlood_group] = useState("");
-  const [personal_contact_number, setPersonal_contact_number] = useState("");
-  const [personal_email_id, setPersonal_email_id] = useState("");
-  const [official_email_id, setOfficial_email_id] = useState("");
-  const [company_id, setCompany_id] = useState("");
+  // const { userId } = useSelector((state) => state);
+  const userId = "63f4a616420ae478e1842a4e";
+  const [data, setData] = useState({});
 
   const navigate = useNavigate();
-  const getData = async () => {
-    try {
-      const response = await AxiosInstance.get(`/api/hr/get/personal/${userId}`)
-      if (response.status === 200) {
-        // console.log(response.data.data[0].personal_details)
-        setPersonal_contact_number(response.data.data[0].personal_details.personal_contact_number);
-        setPersonal_email_id(response.data.data[0].personal_details.personal_email_id);
-        setOfficial_email_id(response.data.data[0].personal_details.official_email_id);
-        setCompany_id(response.data.data[0].personal_details.company_id);
-        setEmployee_code(response.data.data[0].personal_details.employee_code);
-        setReporting_manager(response.data.data[0].personal_details.reporting_manager)
-        setDesignation(response.data.data[0].personal_details.designation);
-        setBlood_group(response.data.data[0].personal_details.blood_group);
-        setEmergency_contact_number(response.data.data[0].personal_details.emergency_contact_number);
-        setName(response.data.data[0].personal_details.name);
-        setGrade(response.data.data[0].personal_details.grade);
-      }
-    } catch (error) {
-      alert(error);
 
-    };
-  };
   const onSubmitClick = () => {
-    navigate(`/hr/edit_profile`)
-  }
+    navigate(`/hr/edit_profile`);
+  };
 
   useEffect(() => {
-    getData();
+    AxiosInstance.get(`/api/hr/get/personal/${userId}`)
+      .then((res) => setData(res.data.data[0].personal_details))
+      .catch((err) => console.log(err));
   }, []);
 
   return (
-    <div className={`${(props.viewViewProfile) ? "block" : "hidden"}`}>
+    <div className={`${props.viewViewProfile ? "block" : "hidden"}`}>
       <div class="overflow-hidden bg-white w-3/4 mx-auto mt-12 shadow-md sm:rounded-lg border border-gray-300">
         <div class="px-4 py-5 sm:px-6">
           <div className="flex justify-between items-center">
             <div>
               <h3 class="text-lg sm:text-xl font-medium leading-6 text-gray-900">
-                {name}
+                {data.name}
               </h3>
-              <p class="mt-1 max-w-2xl text-sm sm:text-md text-gray-500">Personal details</p>
+              <p class="mt-1 max-w-2xl text-sm sm:text-md text-gray-500">
+                Personal details
+              </p>
             </div>
 
-            <img
+            {/* <img
               className="h-16 w-16 sm:h-24 sm:w-24 rounded-full"
-              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              src={data.profile_image}
               alt=""
-            />
+            /> */}
           </div>
         </div>
         <div class="border-t border-gray-300">
@@ -73,23 +46,27 @@ const ViewProfile = (props) => {
             <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
               <dt class="text-sm font-medium text-gray-500">Employee Code</dt>
               <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                {employee_code}
+                {data.employee_code}
               </dd>
             </div>
             <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
               <dt class="text-sm font-medium text-gray-500">Designation</dt>
               <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                {designation}
+                {data.designation}
               </dd>
             </div>
             <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
               <dt class="text-sm font-medium text-gray-500">Grade</dt>
-              <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{grade}</dd>
+              <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                {data.grade}
+              </dd>
             </div>
             <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-gray-500">Reporting Manager</dt>
+              <dt class="text-sm font-medium text-gray-500">
+                Reporting Manager
+              </dt>
               <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                {reporting_manager}
+                {data.reporting_manager}
               </dd>
             </div>
             <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
@@ -97,44 +74,55 @@ const ViewProfile = (props) => {
                 Emergency Contact Number
               </dt>
               <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                {emergency_contact_number}
+                {data.emergency_contact_number}
               </dd>
             </div>
             <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
               <dt class="text-sm font-medium text-gray-500">Blood Group</dt>
-              <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{blood_group}</dd>
+              <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                {data.blood_group}
+              </dd>
             </div>
             <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
               <dt class="text-sm font-medium text-gray-500">
                 Personal Contact Number
               </dt>
               <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                {personal_contact_number}
+                {data.personal_contact_number}
               </dd>
             </div>
             <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-gray-500">Personal Email ID</dt>
+              <dt class="text-sm font-medium text-gray-500">
+                Personal Email ID
+              </dt>
               <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                {personal_email_id}
+                {data.personal_email_id}
               </dd>
             </div>
             <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-gray-500">Official Email ID</dt>
+              <dt class="text-sm font-medium text-gray-500">
+                Official Email ID
+              </dt>
               <dd class="mt-1 t ext-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                {official_email_id}
+                {data.official_email_id}
               </dd>
             </div>
             <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
               <dt class="text-sm font-medium text-gray-500">Company ID</dt>
               <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                {company_id}
+                {data.company_id}
               </dd>
             </div>
           </dl>
         </div>
       </div>
       <div className="w-full flex justify-center">
-        <button onClick={onSubmitClick} className="mx-auto w-11/12 sm:w-3/4 text-white bg-[#047EC1] mt-4 mb-12 border-0 py-2 px-6 focus:outline-none hover:bg-[#0473af] rounded text-lg">Edit Profile</button>
+        <button
+          onClick={onSubmitClick}
+          className="mx-auto w-11/12 sm:w-3/4 text-white bg-[#047EC1] mt-4 mb-12 border-0 py-2 px-6 focus:outline-none hover:bg-[#0473af] rounded text-lg"
+        >
+          Edit Profile
+        </button>
       </div>
     </div>
   );
